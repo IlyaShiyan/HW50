c2587ea985c1d238f1d66e58085c0895b873a331
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-11-23 22:44:08 +0300

    1.1

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,10 +2,11 @@
 public class Animal {
     private String name;
 
-
-    public Animal(String cat) {
+    public Animal(String name) {
+        this.name = name;
     }
 
     @Override
-    public String toString() { return "Animal: name=" + name;}
+    public String toString() { return  name;}
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,32 +2,22 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        Animal name1 = new Animal("Cat");
-        Animal name2 = new Animal("Dog");
-        Animal name3 = new Animal("Fox");
-        Animal name4 = new Animal("Elephant");
-        Animal name5 = new Animal("Monkey");
-        Animal name6 = new Animal("Wolf");
-        Animal name7 = new Animal("Tiger");
-        Animal name8 = new Animal("Lion");
-        Animal name9 = new Animal("Hippo");
-        Animal name10 = new Animal("Hare");
         LinkedList<Animal> listOfAnimals = new LinkedList<>();
-        listOfAnimals.add(name1);
-        listOfAnimals.add(name2);
-        listOfAnimals.add(name3);
-        listOfAnimals.add(name4);
-        listOfAnimals.add(name5);
-        listOfAnimals.add(name6);
-        listOfAnimals.add(name7);
-        listOfAnimals.add(name8);
-        listOfAnimals.add(name9);
-        listOfAnimals.add(name10);
-        
+        listOfAnimals.add(new Animal("Cat"));
+        listOfAnimals.add(new Animal("Dog"));
+        listOfAnimals.add(new Animal("Fox"));
+        listOfAnimals.add(new Animal("Elephant"));
+        listOfAnimals.add(new Animal("Monkey"));
+        listOfAnimals.add(new Animal("Wolf"));
+        listOfAnimals.add(new Animal("Tiger"));
+        listOfAnimals.add(new Animal("Lion"));
+        listOfAnimals.add(new Animal("Hippo"));
+        listOfAnimals.add(new Animal("Hare"));
 
-        for (Animal i:listOfAnimals) {
-            if (i.toString() == "A" || i.toString() =="E" ||i.toString() == "I" ||i.toString() == "O" || i.toString() =="U" ||i.toString() == "Y" ) {
-                listOfAnimals.remove(i);
+
+        for (Animal animal:listOfAnimals) {
+            if (animal. == "A" || animal. =="E".hashCode() ||animal. == "I" ||animal. == "O" || animal. =="U" || animal. == "Y" ) {
+                listOfAnimals.remove(animal);
             }
         }
 
